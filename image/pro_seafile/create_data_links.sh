f89058aff634a4d754bd6837052037d229d44aae
Hu <517046497@qq.com>
2018-07-10 15:04:43 +0800

    help page persistence

--- image/pro_seafile/create_data_links.sh
+++ image/pro_seafile/create_data_links.sh
@@ -41,6 +41,8 @@ done
 current_version_dir=/opt/seafile/seafile-pro-server-${SEAFILE_VERSION}
 latest_version_dir=/opt/seafile/seafile-server-latest
 seahub_data_dir=/shared/seafile/seahub-data
+help_html_dir=/shared/help/html
+help_img_dir=/shared/help/img
 
 if [[ ! -e $latest_version_dir ]]; then
     ln -sf $current_version_dir $latest_version_dir
@@ -57,4 +59,16 @@ ln -sf ${seahub_data_dir}/custom $source_custom_dir
 if [[ ! -e /shared/logs/seafile ]]; then
     mkdir -p /shared/logs/seafile
 fi
+
+if [[ -e $help_html_dir ]]; then
+    seahub_help_dir=$latest_version_dir/seahub/seahub/help/templates/help
+    rm -rf $seahub_help_dir
+    ln -sf $help_html_dir $seahub_help_dir
+fi
+
+if [[ -e $help_img_dir ]]; then
+    media_dir=$latest_version_dir/seahub/media/img/help/
+    cp -f $help_img_dir/* $media_dir
+fi
+
 rm -rf /opt/seafile/logs && ln -sf /shared/logs/seafile /opt/seafile/logs
